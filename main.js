627803c352d0cd1518843270af8dd51cb85c9567
pcatimbang9T <catimbang.phm@obmontessori.edu.ph>
2025-03-18 10:35:10 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function roundOff() {
+	var a = document.getElementById('numA').value;
+	window.alert(Math.round(a))
+}
+function powerOf() {
+	var a = document.getElementById('numA').value;
+	var b = document.getElementById('numB').value;
+	window.alert(Math.pow(a, b));
+}
+function squareRoot() {
+	var a = document.getElementById('numA').value;
+	window.alert(Math.sqrt(a));
+}
+function randomNumber() {
+	window.alert(Math.random());
+}
